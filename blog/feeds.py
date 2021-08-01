09ae18e6bdb0e629624ae7802aa3459006c8e14d
anuran <anuranroy02@gmail.com>
2021-08-01 23:30:33 +0530

    Added Atom feeds

--- blog/feeds.py
+++ blog/feeds.py
@@ -2,9 +2,25 @@ from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords
 from .models import Post
 from django.urls import reverse
+from django.utils.feedgenerator import Atom1Feed
 
+class AtomFeed(Feed):
+    feed_type = Atom1Feed
 
-class PostsFeed(Feed):
+    title = "FOSSBook blog"
+    link = ""
+    description = "Posts from the FOSSBook blog."
+
+    def items(self):
+        return Post.objects.filter(status=1)
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords(item.content, 30)
+
+class RSSFeed(Feed):
     title = "FOSSBook blog"
     link = ""
     description = "Posts from the FOSSBook blog."
--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views, feeds
 urlpatterns = [
     # path('', views.index, name="Index"),
     path('', views.PostList.as_view(), name='home'),
-    path("feed/rss", feeds.PostsFeed(), name="post_feed"),
+    path("feed/rss", feeds.RSSFeed(), name="rss_feed"),
+    path("feed/atom", feeds.AtomFeed(), name="atom_feed"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post'),
 ]
